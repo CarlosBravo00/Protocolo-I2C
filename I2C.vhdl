dde17fb58d8828f6ac0148e5e69958ac7ca0a3b2
CarlosBravo00 <carlosemiliobravomarquez@gmail.com>
2020-06-10 16:05:43 -0500

    Formato

--- I2C.vhdl
+++ I2C.vhdl
@@ -27,10 +27,11 @@ begin
     if (clk'event and clk = '0') then 
     case present is 
         when IDLE =>
-              SDA <= '1';
-              SCL <= '1';
-              shift_add <= "0000000";
-              SHIFT_DAT <= "00000000";
+
+             SDA <= '1';
+             SCL <= '1';
+             shift_add <= "0000000";
+             SHIFT_DAT <= "00000000";
 
             if enable = '1' then
             present <= ADDR;
@@ -40,49 +41,54 @@ begin
             end if;
 
         when ADDR =>
-        SCL <= '0';
-        if incount < x"7" then
-            SDA <= shift_add(6);
-            shift_add(6 downto 0) <= shift_add(5 downto 0) & 'U' ;
-            incount <= incount + 1;
-            present <= tempa;
-         else if incount = x"7" then
-            SDA <= I2C_RW;
-            present <= tempa;
-            incount <= incount + 1;
-        else if  incount < x"A" then
-            incount <= incount + 1;
-            SDA<= '1';
-            present <= ADDR;
+            SCL <= '0';
+            if incount < x"7" then
+                SDA <= shift_add(6);
+                shift_add(6 downto 0) <= shift_add(5 downto 0) & 'U' ;
+                incount <= incount + 1;
+                present <= tempa;
+
+            else if incount = x"7" then
+                SDA <= I2C_RW;
+                present <= tempa;
+                incount <= incount + 1;
+
+            else if  incount < x"A" then
+                incount <= incount + 1;
+                SDA<= '1';
+                present <= ADDR;
+
             else 
-            incount <= x"0";
-            SHIFT_DAT <= I2C_DATA; 
-            present<= DATA;   
-             
+                incount <= x"0";
+                SHIFT_DAT <= I2C_DATA; 
+                present<= DATA;   
+                
                 end if;       
             end if;
          end if;
 
         when data => 
-        SCL <= '0';
-        if incount < x"8" then
-            SDA <= shift_dat(7);
-            shift_dat(7 downto 0) <= shift_dat(6 downto 0) & 'U' ;
-            incount <= incount + 1;
-            present <= tempb;
-        else 
-            incount <= x"0";
-            present <= IDLE;
-        end if;
+            SCL <= '0';
+            if incount < x"8" then
+                SDA <= shift_dat(7);
+                shift_dat(7 downto 0) <= shift_dat(6 downto 0) & 'U' ;
+                incount <= incount + 1;
+                present <= tempb;
+
+            else 
+                incount <= x"0";
+                present <= IDLE;
+                
+            end if;
         
 
         when tempa => 
-        SCL<= '1';
-        present <= ADDR; 
+            SCL<= '1';
+            present <= ADDR; 
 
         when tempb => 
-        SCL<= '1';
-        present <= data; 
+            SCL<= '1';
+            present <= data; 
 
         when others => null; 
         end case;
