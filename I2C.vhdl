1867159bd16342af11a1f151ef8b12810c616d3f
CarlosBravo00 <carlosemiliobravomarquez@gmail.com>
2020-06-10 13:57:23 -0500

    Agregada maquina de estados
    Funciona correctamente leer la addres y leer los datos ingresados

--- I2C.vhdl
+++ I2C.vhdl
@@ -5,48 +5,89 @@ use ieee.numeric_std.all;
 entity I2C is
     port(
         clk: in std_logic;
-        I2C_ADDRESS in std_logic_vector(6 downto 0);
-        I2C_DATA in std_logic_vector(7 downto 0);
-        I2C_RW in std_logic;
+        enable: in std_logic;
+        I2C_ADDRESS: in std_logic_vector(6 downto 0);
+        I2C_DATA: in std_logic_vector(7 downto 0);
+        I2C_RW: in std_logic;
         SDA, SCL : out std_logic
     );
 end entity;
 
 
 architecture arch of I2C is
-    Type State is(IDLE,ADDR,DATA);
+    Type State is(IDLE,ADDR,DATA,tempa,tempb);
     SIGNAL present:state := IDLE;
+    SIGNAL SHIFT_ADD: Std_logic_vector(6 downto 0);
+    SIGNAL SHIFT_DAT: Std_logic_vector(7 downto 0);
     signal incount : unsigned(3 downto 0) := "0000";
 begin
 
-process (I2C_clk_filtered)
+process (clk)
 begin
-if enable = '1' then
-    incount <= x"0";
- else 
-    if (I2C_clk_filtered'event and I2C_clk_filtered = '0') then --rising_edge(filtered)
+    if (clk'event and clk = '0') then 
     case present is 
-
         when IDLE =>
-            present <= RUN;
+              SDA <= '1';
+              SCL <= '1';
+              shift_add <= "0000000";
+              SHIFT_DAT <= "00000000";
 
-      when RUN => -- Shift enl siguientes 8 bits para construir el scan code
-         if incount < x"9" then
-          shift_in(7 downto 0) <= shift_in(8 downto 1);
-          shift_in(8) <= I2C_data;
-          incount <= incount + 1;
-     else 
-        scan_code <= shift_in(7 downto 0);
-        incount <= x"0";
-        present <= IDLE;
+            if enable = '1' then
+            present <= ADDR;
+            shift_add <= I2C_ADDRESS;
+            else 
+            present <= IDLE;
+            end if;
 
-    end if;
-        when others => null; 
-        end case;
+        when ADDR =>
+        SCL <= '0';
+        if incount < x"7" then
+            SDA <= shift_add(6);
+            shift_add(6 downto 0) <= shift_add(5 downto 0) & 'U' ;
+            incount <= incount + 1;
+            present <= tempa;
+         else if incount = x"7" then
+            SDA <= I2C_RW;
+            present <= tempa;
+            incount <= incount + 1;
+        else if  incount < x"A" then
+            incount <= incount + 1;
+            SDA<= '1';
+            present <= ADDR;
+            else 
+            incount <= x"0";
+            SHIFT_DAT <= I2C_DATA; 
+            present<= DATA;   
+             
+                end if;       
+            end if;
          end if;
 
-    end if;
+        when data => 
+        SCL <= '0';
+        if incount < x"8" then
+            SDA <= shift_dat(7);
+            shift_dat(7 downto 0) <= shift_dat(6 downto 0) & 'U' ;
+            incount <= incount + 1;
+            present <= tempb;
+        else 
+            incount <= x"0";
+            present <= IDLE;
+        end if;
+        
+
+        when tempa => 
+        SCL<= '1';
+        present <= ADDR; 
+
+        when tempb => 
+        SCL<= '1';
+        present <= data; 
+
+        when others => null; 
+        end case;
+
+         end if;
    end process; 
    
-
 end arch ; -- arch
--- I2C_tb.vhdl
+++ I2C_tb.vhdl
@@ -8,15 +8,26 @@ architecture arch of I2C_tb is
 
     component I2C is
         port(
-          
+            clk: in std_logic;
+            enable: in std_logic;
+            I2C_ADDRESS: in std_logic_vector(6 downto 0);
+            I2C_DATA: in std_logic_vector(7 downto 0);
+            I2C_RW: in std_logic;
+            SDA, SCL : out std_logic
         );
     end component;
-
-    constant period : time := 10 ns;
+    signal clk : std_logic := '0';
+    signal enable : std_logic := '0';
+    signal I2C_ADDRESS : std_logic_vector(6 downto 0);
+    signal I2C_DATA : std_logic_vector(7 downto 0);
+    signal I2C_RW :std_logic;
+    signal SDA :std_logic;
+    signal SCL :std_logic;
+    constant period : time := 40 ns;
 
 begin
 
-    UUT : I2C port map ();
+    UUT : I2C port map (CLK,enable,I2C_ADDRESS,I2C_DATA,I2C_RW,SDA,SCL);
 
     clk_process : process
     begin
@@ -26,4 +37,8 @@ begin
         wait for period/2;
     end process;
 
+    enable <= '1', '0' after 1500 ns;
+    I2C_ADDRESS <= "0001011";
+    i2C_DATA <= "11001100";
+    I2C_RW <= '0';
 end arch;
