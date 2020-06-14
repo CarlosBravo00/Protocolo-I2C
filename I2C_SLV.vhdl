d08c6d0661ba44ca19a792ab1eaf21a3b931344c
CarlosBravo00 <carlosemiliobravomarquez@gmail.com>
2020-06-14 12:30:02 -0500

    Esclavo funcional

--- I2C_SLV.vhdl
+++ I2C_SLV.vhdl
@@ -4,12 +4,12 @@ use ieee.numeric_std.all;
 
 entity I2C_SLV is
     port(
-        I2C_ADDRESS: in std_logic_vector(6 downto 0); --Direccio del esclavo 
+        I2C_ADDRESS: in std_logic_vector(6 downto 0); --Direccion del esclavo 
         I2C_DATA: in std_logic_vector(7 downto 0); --Data en el esclavo
         DATA_WRITE: out std_logic_vector(7 downto 0); --Data que recive el esclavo
         SCL: in std_logic;--SCL = Serial Clock 
         SDA : inout std_logic; --SDA = Serial Data/Address  
-        SLV_BUSY : out std_logic --1 Busy,0 Espera respuesta
+        SLV_BUSY: out std_logic := '1'  --1 Busy,0 Espera respuesta
         
     );
 end entity;
@@ -27,86 +27,70 @@ process(SCL)
 begin 
     if (SCL'event and SCL = '0') then 
     
-            case present is 
-            when IDLE => --Estado inicial 
-                    
-            SLV_BUSY <= '1';
-
-                if SDA  = '1' then --BIT INICIO 
+    case present is 
+        when IDLE => --Estado inicial      
+                SLV_BUSY <= '1';
+                if SDA  = '0' then --BIT INICIO 
                     present <= ADDR;
-                    shift_add <= "UUUUUUU";
-                    SIG_rW <= 'U';
+                    SIG_RW <= 'U';
                 else 
                     present <= IDLE;
                 end if;
 
         when ADDR => --Direccion y RW
 
-        if incount < x"7" then --Direccion 7 bits   
-            shift_add(6 downto 0) <= shift_add(6 downto 0) & SDA;
-            incount <= incount + 1;
-            present <= ADDR; 
+                if incount < x"7" then --Direccion 7 bits   
+                    shift_add(6 downto 0) <= shift_add(5 downto 0) & SDA;
+                    incount <= incount + 1;
+                    present <= ADDR; 
 
-        else if incount = x"7" then --RW 1 bit 
-            if (shift_add = I2C_ADDRESS) then 
-        --SI LA DIRECCION SENT NO COINCIDE CON LA DEL ESCLAVO REGRESA A IDDLE            
-            SIG_RW <= SDA;
-            incount <= incount + 1;
-            present <= ADDR;
-            else 
-            present <= IDLE;
-            end if;
+                else if incount = x"7" then --RW 1 bit 
+                    if (shift_add = I2C_ADDRESS) then 
+                --SI LA DIRECCION SENT NO COINCIDE CON LA DEL ESCLAVO REGRESA A IDDLE            
+                    SIG_RW <= SDA;
+                    incount <= incount + 1;
+                    present <= ADDR;
+                    SLV_BUSY <= '0';
+                    else 
+                    present <= IDLE;
+                    end if;
 
-        else if incount = x"8" then --ACK
-            SLV_BUSY <= '0';
-            SDA<= '1'; --MAYBE ERROR
-            present <= ADDR;
-
-        else 
-            SLV_BUSY <= '1';
-            incount <= x"0";
-
-                if SIG_RW = '0' then --Write Data
-                    present<= WDATA;   
-                else  --Read Data
-                    SLV_BUSY <= '0'; 
-                    present<= RDATA;
-                end if;
+                else --ACK
+                    SDA<= '1';
+                    incount <= x"0";
+                    if SIG_RW = '0' then --Write Data
+                        SLV_BUSY <= '1';
+                        shift_add <= "UUUUUUU";
+                        present<= WDATA; 
+                    else  --Read Data
+                        SLV_BUSY <= '0';
+                        shift_dat <= I2C_DATA;
+                        present<= RDATA;
+                    end if;
 
                end if;       
             end if;
-        end if;
-
 
         when WDATA => 
-        if incount < x"8" then --Escribir datos 8 bits 
-            SHIFT_DAT(7 downto 0) <= shift_dat(7 downto 0) & SDA;
-            incount <= incount + 1;
-            present <= WDATA;
+            if incount < x"8" then --Escribir datos 8 bits 
+                SHIFT_DAT(7 downto 0) <= shift_dat(6 downto 0) & SDA;
+                incount <= incount + 1;
+                present <= WDATA;
 
-        else if incount = x"8" then --ACK
-            DATA_WRITE <=  SHIFT_DAT;
-            SLV_BUSY <= '0';
-            SDA<= '1'; --Puede fallar
-            present <= WDATA;
-
-            else  --Stop Regresa a IDLE 
-                SLV_BUSY <= '1';   
-            if (SDA = '1') then --stop bit 
-                    incount <= x"0";
-                    present <= IDLE;
-                else
-                    present <= WDATA;
-
+            else if incount = x"8" then --ACK
+                DATA_WRITE <=  SHIFT_DAT;
+                SLV_BUSY <= '0';
+                SDA<= '1'; 
+                incount <= x"0";
+                present <= IDLE;
                 end if;
-            end if;
-        end if;   
+            end if;   
 
 
         WHEN RDATA => --Read data
             if incount < x"8" then --Lecutra de la data manda esclavo
-                SDA <= shift_dat(6);
-                shift_dat(6 downto 0) <= shift_dat(5 downto 0) & 'U' ;
+                SDA <= shift_dat(7);
+                shift_dat(7 downto 0) <= shift_dat(6 downto 0) & 'U' ;
                 incount <= incount + 1;
                 present <= RDATA;
 
@@ -116,7 +100,6 @@ begin
                 present <= RDATA;
 
             else  --STOP regresa a iddle 
-                SLV_BUSY <= '1';   
                 if (SDA = '1') then --stop bit 
                         incount <= x"0";
                         present <= IDLE;
@@ -130,13 +113,7 @@ begin
         when others => null; 
         end case;
 
-    end if;
+         end if;
 
-end process; 
-
-
-
-
-
-
+    end process; 
 end arch ; --arch
--- I2C_SLV_tb.vhdl
+++ I2C_SLV_tb.vhdl
@@ -1,10 +1,10 @@
 library ieee;
 use ieee.std_logic_1164.all;
 
-entity I2C_tb is
+entity I2C_SLV_tb is
 end entity;
 
-architecture arch of I2C_tb is
+architecture arch of I2C_SLV_tb is
 
     component I2C_SLV is
         port(
@@ -20,40 +20,62 @@ architecture arch of I2C_tb is
     signal I2C_DATA : std_logic_vector(7 downto 0);
     signal SDA :std_logic;
     signal SCL :std_logic;
-    signal SLV_BUSY : std_logic;
+    signal SLV_BUSY : std_logic := '1';
     signal DATA_WRITE:  std_logic_vector(7 downto 0);
 
     signal SENT_ADDRESS : std_logic_vector (6 downto 0);
-    signal SENT_DATA : std_logic_Vector (7 downto 0)
+    signal SENT_DATA : std_logic_Vector (7 downto 0);
     signal SENT_RW : std_logic;
+    signal DATA_MASTER : std_logic;
     constant period : time := 10 us;
 
 begin
 
-    UUT : I2C_tb port map (I2C_ADDRESS,I2C_DATA,DATA_WRITE,SCL,SDA,SLV_BUSY);
+    UUT : I2C_SLV port map (I2C_ADDRESS,I2C_DATA,DATA_WRITE,SCL,SDA,SLV_BUSY);
 
 process
     begin
-        SCL <= 'H';
-        DATA_SLV  <= 'H';
-
+        wait for (period / 2);
+        SCL <= '1'; 
+        DATA_MASTER <= '1';
         wait for (period / 2);
         -- start bit
-        DATA_SLV <= '1';
-        wait for (bit_period / 2);
+        DATA_MASTER <= '0';
+        wait for (period / 2);
         SCL <= '0'; 
-        wait for (bit_period / 2);
-        SCL <= '1';
-
-        wait until SLV_BUSY'event and SLV_BUSY = '0'; --ACK de Address
-        wait for period;
-        wait until SLV_BUSY'event and SLV_BUSY = '0' and I2C_RW = '1';
-        for i in 0 to 7 loop
-            DATA_SLV<= RD_DATA(7-i); --Si es lectura manda la informacion de leer
-            wait until SCL'event and SCL = '0';
+        wait for (period / 2);
+        for i in 0 to 6 loop --Address
+            DATA_MASTER<= SENT_ADDRESS(6-i); 
+            wait for (period / 2);
+            SCL <= '1'; 
+            wait for (period / 2);
+            SCL <= '0'; 
         end loop;
-
-
+        DATA_MASTER <= SENT_RW;
+        wait for (period / 2);
+        SCL <= '1'; 
+        wait for (period / 2);
+        SCL <= '0'; 
+        wait for (period / 2);
+        SCL <= '1'; 
+        wait for (period / 2);
+        SCL <= '0'; 
+        wait for period;
+        for i in 0 to 7 loop --Data
+            DATA_MASTER<= SENT_DATA(7-i); 
+            wait for (period / 2);
+            SCL <= '1'; 
+            wait for (period / 2);
+            SCL <= '0'; 
+       end loop;
+       DATA_MASTER<='1';
+        wait for (period / 2);
+        SCL <= '1'; 
+        wait for (period / 2);
+        SCL <= '0'; 
+        wait for (period / 2);
+        SCL <= '1'; 
+        wait for (period*2);
         wait;
     end process;
 
@@ -61,9 +83,10 @@ process
 
     I2C_ADDRESS <= "0110001";
     I2C_DATA <= "01111010";
-
     SENT_ADDRESS <= "0110001";
-    SENT_DATA <= "110100010";
-    SENT_RW <= '1';
+    SENT_DATA <= "11010001";
+    SENT_RW <= '0';
+
+    --Output = DATA_WRITE
 
 end arch;
