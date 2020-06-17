e2046783fc3e9e2105b4f26f63a7b5d8a9ae3794
CarlosBravo00 <carlosemiliobravomarquez@gmail.com>
2020-06-16 19:19:44 -0500

    Master Simulacion
    agregada lectura de un archivo y escritura en la consola

--- Controlador Master/I2C_tb.vhdl
+++ Controlador Master/I2C_tb.vhdl
@@ -1,6 +1,10 @@
 library ieee;
 use ieee.std_logic_1164.all;
 
+library std;
+use std.textio.all;
+use ieee.std_logic_textio.all;
+
 entity I2C_tb is
 end entity;
 
@@ -34,7 +38,9 @@ architecture arch of I2C_tb is
     signal RD_DATA : std_logic_vector(7 downto 0);
     constant period : time := 10 us; --100khz *Estandar
 
+
 begin
+
 
     UUT : I2C port map (CLK,enable,reset,I2C_ADDRESS,I2C_DATA,I2C_RW,SDA,SCL,I2C_BUSY,DATA_READ);
 
@@ -47,25 +53,55 @@ process
         wait until I2C_BUSY'event and I2C_BUSY = '0'; --ACK de Address
         wait for period;
         wait until I2C_BUSY'event and I2C_BUSY = '0' and I2C_RW = '1';
---SIMULACION DEL ESCLAVO--READ---------------------------------------------------
+--SIMULACION DEL ESCLAVO--READ---
         for i in 0 to 7 loop
             DATA_SLV<= RD_DATA(7-i); --Si es lectura manda la informacion de leer
             wait until SCL'event and SCL = '0';
         end loop;
-
-
         wait;
     end process;
 
-    SDA <= DATA_SLV when (I2C_BUSY = '0') else 'Z';
-    enable <= '1' after 4*period, '0' after 8*period;
-    
-    I2C_ADDRESS <= "0110001";
-    I2C_DATA <= "01111010";
-    I2C_RW <= '1';
-    RD_DATA <= "11010010";
+process 
+     variable l : line;
+     file fin : TEXT open READ_MODE is "Masterinput.txt";
+     variable current_read_line : line;
+     variable current_read_field : string(1 to 4);
+     variable current_read_data : std_logic;
+     variable current_read_dataADD : std_logic_vector(6 downto 0);
+     variable current_read_dataDAT : std_logic_vector(7 downto 0);
+     variable current_write_line : line;
+    begin
+        readline(fin, current_read_line);
+        read(current_read_line, current_read_field);
+        read(current_read_line, current_read_dataADD);
+            I2C_ADDRESS <= current_read_dataADD; 
+        readline(fin, current_read_line);
+        read(current_read_line, current_read_field);
+        read(current_read_line, current_read_data);
+            I2C_RW  <= current_read_data;
+        readline(fin, current_read_line);
+        read(current_read_line, current_read_field);
+        read(current_read_line, current_read_dataDAT);
+            I2C_DATA <= current_read_dataDAT;
+        wait for  4*period;
+        enable <= '1';
+        wait for  4*period;
+        enable <= '0';
 
-    --Output consola = DATA_READ
-------------------------------------------------------------
+        if (I2C_RW = '1') then    
+            wait until DATA_READ /= "UUUUUUUU";
+                write (l, string'("DATA READ: "));
+                write (l, DATA_READ);
+                writeline(output, l);
+        end if;
+        wait for 300* period;
+    end process;
+
+    SDA <= DATA_SLV when (I2C_BUSY = '0') else 'Z';
+   -- enable <= '1' after 4*period, '0' after 8*period;
+    RD_DATA <= "11010010"; --Slave data 
+    --I2C_ADDRESS <= "0110001";
+    --I2C_DATA <= "01111010";
+    --I2C_RW <= '1';
 
 end arch;
